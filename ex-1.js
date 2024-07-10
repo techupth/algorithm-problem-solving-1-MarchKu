cae717b08041eb4a0aa672dca60cb0506858918f
Kunakorn <kunakorn.ph98@gmail.com>
2024-07-10 14:32:19 +0700

    March

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,16 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  let number = {};
+  for (let i in numbers) {
+    number[numbers[i]] = Number(i);
+  }
+	for(let i in numbers){
+    if(number[target-numbers[i]] && number[target-numbers[i]] != i){
+      return [Number(i),number[target-numbers[i]]]
+    }
+  } 
+}
+numbers1 = [2, 7, 11, 15]; // input 9 output [0, 1]
+numbers2 = [2, 7, 11, 15]; // input 23 output []
+
+console.log(twoSum(numbers1, 9));
+console.log(twoSum(numbers2, 23));
